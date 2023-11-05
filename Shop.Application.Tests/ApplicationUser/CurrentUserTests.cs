010cd34ecd761623eac4afd195b5ce8b11f3b2eb
Dominik <lendominik13@gmail.com>
2023-11-05 13:09:40 +0100

    Create CurrentUser and UserContext tests

---
+++ Shop.Application.Tests/ApplicationUser/CurrentUserTests.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+using Shop.Application.ApplicationUser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace Shop.Application.ApplicationUser.Tests
+{
+    public class CurrentUserTests
+    {
+        [Fact()]
+        public void IsInRole_WithMatchingRole_ShouldReturnTrue()
+        {
+            var currentUser = new CurrentUser("1", "test@tests.com", new List<string> { "Admin", "User"});
+
+            var isInRole = currentUser.IsInRole("Admin");
+
+            isInRole.Should().BeTrue();
+        }
+
+        [Fact()]
+        public void IsInRole_WithNoMatchingRole_ShouldReturnFalse()
+        {
+            var currentUser = new CurrentUser("1", "test@tests.com", new List<string> { "Admin", "User" });
+
+            var isInRole = currentUser.IsInRole("Owner");
+
+            isInRole.Should().BeFalse();
+        }
+
+        [Fact()]
+        public void IsInRole_WithNoMatchingCaseRole_ShouldReturnFalse()
+        {
+            var currentUser = new CurrentUser("1", "test@tests.com", new List<string> { "Admin", "User" });
+
+            var isInRole = currentUser.IsInRole("admin");
+
+            isInRole.Should().BeFalse();
+        }
+    }
+}
---
+++ Shop.Application.Tests/ApplicationUser/UserContextTests.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+using Shop.Application.ApplicationUser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using FluentAssertions;
+
+namespace Shop.Application.ApplicationUser.Tests
+{
+    public class UserContextTests
+    {
+        [Fact()]
+        public void GetCurrentUser_WithAuthenticatedUser_ShouldReturnCurrentUser()
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, "1"),
+                new Claim(ClaimTypes.Email, "test@example.com"),
+                new Claim(ClaimTypes.Role, "Admin"),
+                new Claim(ClaimTypes.Role, "User")
+            };
+
+            var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "Test"));
+
+            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+           
+            httpContextAccessorMock.Setup(x => x.HttpContext).Returns(new DefaultHttpContext()
+            {
+                User = user
+            });
+
+            var userContext = new UserContext(httpContextAccessorMock.Object);
+
+            var currentUser = userContext.GetCurrentUser();
+
+            currentUser.Should().NotBeNull();
+            currentUser!.Id.Should().Be("1");
+            currentUser.Email.Should().Be("test@example.com");
+            currentUser.Roles.Should().ContainInOrder("Admin", "User");
+
+        }
+    }
+}
