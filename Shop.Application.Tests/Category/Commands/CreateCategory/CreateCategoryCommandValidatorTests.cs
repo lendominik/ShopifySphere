40b83ccd0e9498053be318807321e7858a556492
Dominik <lendominik13@gmail.com>
2023-11-05 14:39:35 +0100

    CreateCategoryCommandValidator tests added

---
+++ Shop.Application.Tests/Category/Commands/CreateCategory/CreateCategoryCommandValidatorTests.cs
@@ -0,0 +1,54 @@
+﻿using Xunit;
+using Shop.Application.Category.Commands.CreateCategory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Shop.Application.Item.Commands.CreateItem;
+using Shop.Domain.Interfaces;
+using FluentValidation.TestHelper;
+
+namespace Shop.Application.Category.Commands.CreateCategory.Tests
+{
+    public class CreateCategoryCommandValidatorTests
+    {
+        [Fact()]
+        public void Validate_WithValidCommand_ShouldNotHaveValidationError()
+        {
+            var categoryRepositoryMock = new Mock<ICategoryRepository>();
+
+            var validator = new CreateCategoryCommandValidator(categoryRepositoryMock.Object);
+
+            var command = new CreateCategoryCommand()
+            {
+                Description = "description",
+                EncodedName = "category",
+                Name = "name"
+            };
+
+            var result = validator.TestValidate(command);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+        [Fact()]
+        public void Validate_WithValidCommand_ShouldHaveValidationError()
+        {
+            var categoryRepositoryMock = new Mock<ICategoryRepository>();
+
+            var validator = new CreateCategoryCommandValidator(categoryRepositoryMock.Object);
+
+            var command = new CreateCategoryCommand()
+            {
+                Description = "den",
+                EncodedName = "category",
+            };
+
+            var result = validator.TestValidate(command);
+
+            result.ShouldHaveValidationErrorFor(i => i.Name);
+            result.ShouldHaveValidationErrorFor(i => i.Description);
+        }
+    }
+}
