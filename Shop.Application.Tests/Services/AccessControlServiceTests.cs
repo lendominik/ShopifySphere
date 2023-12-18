3c0642e8cc22d9cc4d38f1b9c3b3803903151c58
Dominik <138286618+lendominik@users.noreply.github.com>
2023-12-18 22:20:29 +0100

    Added tests for AccessControlService and updated existing tests (#12)

---
+++ Shop.Application.Tests/Services/AccessControlServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using Moq;
+using Shop.Application.ApplicationUser;
+using Xunit;
+
+namespace Shop.Application.Services.Tests
+{
+    public class AccessControlServiceTests
+    {
+        [Fact]
+        public void IsEditable_ReturnsTrueForOwnerRole_ReturnsTrue()
+        {
+            // Arrange
+            var currentUser = new CurrentUser("1", "test@tests.com", new List<string> { "Owner" });
+            var userContextMock = new Mock<IUserContext>();
+            userContextMock.Setup(u => u.GetCurrentUser()).Returns(currentUser);
+
+            var accessControlService = new AccessControlService(userContextMock.Object);
+
+            // Act
+            var isEditable = accessControlService.IsEditable();
+
+            // Assert
+
+            isEditable.Should().BeTrue();
+        }
+        [Fact]
+        public void IsEditable_ReturnsTrueForOwnerRole_ReturnsFalse()
+        {
+            // Arrange
+            var currentUser = new CurrentUser("1", "test@tests.com", new List<string> { "Admin" });
+            var userContextMock = new Mock<IUserContext>();
+            userContextMock.Setup(u => u.GetCurrentUser()).Returns(currentUser);
+
+            var accessControlService = new AccessControlService(userContextMock.Object);
+
+            // Act
+            var isEditable = accessControlService.IsEditable();
+
+            // Assert
+
+            isEditable.Should().BeFalse();
+        }
+    }
+}
