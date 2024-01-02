e5bbba362598ca1505e9f7274e0a60cd6a35dc19
lendominik <len.dominik13@gmail.com>
2024-01-02 10:54:06 +0100

    Domain tests created

---
+++ Shop.DomainTests/Entities/CategoryTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.Entities.Tests
+{
+    [TestClass()]
+    public class CategoryTests
+    {
+        [TestMethod()]
+        public void EncodeNameTest()
+        {
+
+        }
+    }
+}
